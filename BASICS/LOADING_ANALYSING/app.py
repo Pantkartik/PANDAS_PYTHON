aa3aff30aa6d4ec5dbb8cadeeaaddfff8f31ea91
Kartik Pant <kartikpant.kp69@gmail.com>
2025-09-03 21:00:55 +0530

    Update app.py

--- BASICS/LOADING_ANALYSING/app.py
+++ BASICS/LOADING_ANALYSING/app.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 # df = pd.read_csv("C:/Users/naman/OneDrive/Documents/GitHub/PANDAS_PYTHON/BASICS/LOADING_ANALYSING/data.csv")
  
-data=pd.read_csv(r'C:\Users\naman\OneDrive\Documents\GitHub\PANDAS_PYTHON\BASICS\LOADING_ANALYSING\data.csv')
+data=pd.read_csv(r'C:\Users\naman\OneDrive\Documents\GitHub\PANDAS_PYTHON\BASICS\LOADING_ANALYSING\data.csv',encoding="latin1")
 
+# both methods
+# data=pd.read_csv(r'C:\Users\naman\OneDrive\Documents\GitHub\PANDAS_PYTHON\BASICS\LOADING_ANALYSING\data.csv')
 print(data)
